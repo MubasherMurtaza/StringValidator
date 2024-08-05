7f1d95f0e26a4d8d0db0bdccc3b9bac43b7014ee
Selteq Ltd <hameed@selteq.net>
2024-08-05 12:08:16 +0500

    add files

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Mubasher Murtaza-Pc on 05/08/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
